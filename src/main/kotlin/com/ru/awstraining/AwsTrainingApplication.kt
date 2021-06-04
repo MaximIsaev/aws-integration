f16dd1555dae73ca5cae3db693815883e54a1260
Maxim Isaev <maxim_isaev1@epam.com>
2021-06-04 08:30:41 +0300

    first commit

---
+++ src/main/kotlin/com/ru/awstraining/AwsTrainingApplication.kt
@@ -0,0 +1,11 @@
+package com.ru.awstraining
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AwsTrainingApplication
+
+fun main(args: Array<String>) {
+	runApplication<AwsTrainingApplication>(*args)
+}
---
+++ src/test/kotlin/com/ru/awstraining/AwsTrainingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ru.awstraining
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AwsTrainingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
